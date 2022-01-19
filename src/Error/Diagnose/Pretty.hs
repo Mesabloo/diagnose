63bec43b9c0e06fc9119f58717b1cc0207605800
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2022-01-19 09:14:57 +0100

    pretty: re-export Prettyprinter

--- src/Error/Diagnose/Pretty.hs
+++ src/Error/Diagnose/Pretty.hs
@@ -1,4 +1,4 @@
 module Error.Diagnose.Pretty (module Export) where
 
-import qualified Prettyprinter as Export (Pretty(..), Doc, emptyDoc, viaShow, nest, line, line', softline, softline', hardline, group, flatAlt, align, hang, indent, encloseSep, list, tupled, (<>), (<+>), concatWith, hsep, vsep, fillSep, sep, hcat, vcat, fillCat, cat, punctuate, column, nesting, width, fill, fillBreak, plural, enclose, surround, squotes, dquotes, parens, angles, brackets, braces, lparen, rparen, langle, rangle, lbrace, rbrace, lbracket, rbracket, squote, dquote, semi, colon, comma, space, dot, slash, backslash, equals, pipe, annotate)
-import qualified Prettyprinter.Render.Terminal as Export (AnsiStyle, Color(..), color, colorDull, bgColor, bgColorDull, bold, italicized, underlined, Intensity(..))
+import qualified Prettyprinter as Export 
+import qualified Prettyprinter.Render.Terminal as Export 
